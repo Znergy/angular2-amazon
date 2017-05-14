78e8d2793f287ddff1e860043f40059ae495c568
Ryan <ryanjonesirl@gmail.com>
2017-05-13 22:54:27 -0700

    add the product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  providers: [ ProductService ]
 })
 export class ProductComponent implements OnInit {
+  productId: string;
+  productToDisplay;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private productService: ProductService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+     this.productId = urlParameters['id'];
+   });
+   this.productToDisplay = this.productService.getProductById(this.productId);
   }
 
 }
