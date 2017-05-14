04bc56d0185a6ef36106af2c73650715ddd46642
Ryan <ryanjonesirl@gmail.com>
2017-05-13 22:55:14 -0700

    add the products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
+  providers: [ ProductService ]
 })
 export class ProductsComponent implements OnInit {
+  products: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit() {
+    this.products = this.productService.getProducts();
   }
+
+  goToDetailPage(clickedProduct) {
+    this.router.navigate(['products', clickedProduct.$key]);
+  };
 
 }
